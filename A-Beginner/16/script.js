b5f71bf288884de3bbba7de7905c57b97fce8ac9
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-17 13:37:28 +0200

    16

---
+++ A-Beginner/16/script.js
@@ -0,0 +1,17 @@
+//  Skriv en funktion checkAge som accepterar ett tal age som argument och returnerar strängen "old" 
+// om age är större än 65 annars returnerar "young"
+
+
+ 
+ function checkAge(age) {
+    if (age < 65) {
+        return ("young")
+    }
+    else {
+        return ("old")
+    }
+
+ }
+
+ console.log (checkAge(66))
+
