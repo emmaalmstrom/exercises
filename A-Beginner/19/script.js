2b0c1787ea1e09fb5fcd452ea88ea6b67d7d6367
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 17:57:12 +0200

    19

---
+++ A-Beginner/19/script.js
@@ -0,0 +1,14 @@
+// 19. Skriv en funktion ``isLong`` som accepterar en string och returnerar 
+//`true` ifall den är längre än 18 tecken annars `false`
+
+
+function isLong(str){
+    return str.length > 10
+}
+
+console.log('hej')
+console.log(isLong('hejsan mitt namn är emma'))
+console.log('hje2')
+console.log(isLong('hej'))
+console.log('hej3')
+console.log(isLong('jnwneoewnoinwionoweniiwoen'))
