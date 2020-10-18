2fc284d8e52fcfe811b8a281eb48bdd1000aea26
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 18:10:08 +0200

    21

---
+++ A-Beginner/21/script.js
@@ -0,0 +1,9 @@
+// 21. Skriv en funktion `isEven` som returnerar `true` 
+//om argumentet är jämnt och `false` om argumentet är udda
+
+function isEven(a){
+    return a % 2 === 0
+}
+
+console.log(isEven(13))
+console.log(isEven(14))
