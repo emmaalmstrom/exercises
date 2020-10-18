f296d85b9bf129d97a4064f2a310cee09c14a7c0
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 18:06:31 +0200

    20

---
+++ A-Beginner/20/script.js
@@ -0,0 +1,8 @@
+// 20. Skriv en funktion som inte tar något argument och genererar ett random tal mellan 0 och 5
+
+function randomNumber (){
+    return Math.random() * 5
+}
+
+console.log(randomNumber())
+console.log(randomNumber())
