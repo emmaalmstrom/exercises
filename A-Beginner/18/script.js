616835ae36cba817c9410e2bc948602af6318646
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 17:25:27 +0200

    18

---
+++ A-Beginner/18/script.js
@@ -0,0 +1,9 @@
+// 18. Skriv en funktion som kollar ifall två strängar
+// är lika (och returnerar `true` i så fall, annars `false`)
+
+function isSame(a, b){
+    return a === b
+}
+
+console.log(isSame('hej', 'hje'))
+console.log(isSame('hej', 'hej'))
