cf288ab87667c5bed2113ace5458b8a1f2584307
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 20:31:08 +0200

    23

---
+++ A-Beginner/23/script.js
@@ -0,0 +1,9 @@
+// 23. Skriv en funktion `isDivisibleByFive` som returnerar `true`/`false` om argumentet är delbart med 5
+
+function isDivisibleByFive(a){
+    return a % 5 === 0
+}
+
+console.log(isDivisibleByFive(30))
+console.log(isDivisibleByFive(32))
+console.log(isDivisibleByFive(322))
