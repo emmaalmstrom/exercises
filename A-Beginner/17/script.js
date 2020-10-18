0dc299eb5dd66ccdee66e42d0bd54c03039ab32f
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 17:19:47 +0200

    17

---
+++ A-Beginner/17/script.js
@@ -0,0 +1,10 @@
+// 17. Skriv en funktion `isTanja` som returnerar `true` ifall´
+// argumentet är strängen "Tanja"  och annars `false`
+
+function isTanja(x) {
+    return x === 'Tanja'
+}
+
+console.log(isTanja('Ilona'))
+console.log(isTanja('Zlatan'))
+console.log(isTanja('Tanja'))
---
+++ A-Beginner/17/script2.js
@@ -0,0 +1,9 @@
+// 17. Skriv en funktion `isTanja` som returnerar `true` ifall´
+// argumentet är strängen "Tanja"  och annars `false`
+
+function isTanja(x){
+    return x === 'Tanja'
+}
+
+console.log(isTanja('Hej'))
+console.log(isTanja('Tanja'))
