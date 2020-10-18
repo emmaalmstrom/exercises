f1dfd0624c2a10efc030e8ebb55c319587ea4646
Emma Almström <emma.almstrom@skola.botkyrka.se>
2020-10-18 20:44:01 +0200

    25

---
+++ A-Beginner/25/script.js
@@ -0,0 +1,10 @@
+// 25. Skriv en funktion `isLagom` som returnerar `true` ifall 
+// argumentet är ett tal som är mellan 8 och 16 och annars returnerar `false`
+
+function isLagom(x){
+    return 7 < x && x < 17
+}
+
+console.log(isLagom(10))
+console.log(isLagom(101))
+console.log(isLagom(1))
